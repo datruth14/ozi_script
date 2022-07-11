9716a5b8b3102d34b0c8f86e04820d5c978520b7
datruth14 <datruth14.vd@gmail.com>
2022-07-11 01:26:43 +0100

    ioi

--- ui_config.php
+++ ui_config.php
@@ -7,6 +7,8 @@
   <meta name="description" content="A new ozi script app">
   <meta name="keyword" content="MyApp">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="icon" type="image/x-icon" href="assets/media/images/icons/favicon.ico">
+
   <!-- iOS meta tags & icons -->
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
