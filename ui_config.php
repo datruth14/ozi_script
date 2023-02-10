29a6800dd8cacb83972d5cff59ff8b2198a63ee7
Victor Francis <datruth14.vd@gmail.com>
2023-02-10 15:18:30 +0100

    update

--- ui_config.php
+++ ui_config.php
@@ -52,14 +52,19 @@
 </head>
 
 <body id="oziApp" style="margin:0;">
+   <div id="loader">
+    </div>
+
     <section id="root">
         <?php
         //checking if linking is set
         if (isset($_GET['screens'])) {
             //checking if screen exist
             if (file_exists("screens/" . $_GET['screens'] . "")) {
+                echo "<style>#loader{display:none;}</style>";
                 require "screens/" . $_GET['screens'] . "";
             } else {
+                echo "<style>#loader{display:none;}</style>";
                 echo "<center><br><Br><br><h1 style='color:red;'><strong><i class='fa fa-spider'></i>Error 404</strong></h1><dp style='color:white;'>Opps Screen not found</dp></center><br><br>";
             }
         } else {
