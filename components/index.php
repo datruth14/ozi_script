c30e247071177beeb4ae3936840a463eb476e0fd
Victor Francis <39800735+datruth14@users.noreply.github.com>
2023-07-04 11:50:17 +0100

    jj

--- components/index.php
+++ components/index.php
@@ -3,12 +3,13 @@
 <div class="px-4 py-5 my-5  text-center">
     <h1 class="display-5 fw-bold animate__animated animate__rubberBand animate__delay-2s	2s animate__infinite animate__slower 3s">Congratulations!</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4" style="color:purple;font-weight:430;">You have successfully downloaded and installed ozi script project structure, Let's start by editing the index.php component inside your project directory, "components/index.php". <a href="https://ozi.com.ng/?screens=doc.ozi"> Learn More </a></p>
+      <p class="lead mb-4" style="color:purple;font-weight:430;">You have successfully downloaded and installed ozi script project structure, Let's start by editing the index.php component inside your project directory. <a href="#"> Learn More </a></p>
 
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <a  href="" class="btn btn-primary btn-lg px-4 gap-3" style="border-radius:25px;"> Get Started With Widgets  </a>      
+
+      <a  href="#"  class="btn btn-primary btn-lg px-4 gap-3" style="border-radius:25px;"> Get Started With Widgets  </a>      
 
       
     </div>
-    </div>ç
+    </div>
 </div>
