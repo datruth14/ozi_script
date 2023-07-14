687f72e238d3da7dd1b76066d5e94fa0222254f2
Victor Francis <39800735+datruth14@users.noreply.github.com>
2023-07-14 03:09:55 +0100

    new

--- ui_config.php
+++ ui_config.php
@@ -8,18 +8,8 @@
     <meta name="keyword" content="Build web app with ease using ozi script">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 
-    <!-- iOS meta tags & icons -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="Ozi Script">
-    <meta name="msapplication-TileColor" content="#da532c">
-    
-    <!--IOS App ICONS Config -->
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/media/images/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/media/images/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/media/images/icons/favicon-16x16.png">
-    <link rel="manifest" href="assets/media/images/icons/site.webmanifest">
-    <link rel="mask-icon" href="assets/media/images/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <!--Calling IOS Media Dependency -->
+    <?php iosMedia()?>
 
     <!-- Main App Manifest Config -->
     <link rel="manifest" href="/manifest.json">
