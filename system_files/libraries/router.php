7901bb6a4f8d9f80de542ccf8e7885ac21879610
Victor Francis <39800735+datruth14@users.noreply.github.com>
2024-08-13 16:06:18 +0100

    22

--- system_files/libraries/router.php
+++ system_files/libraries/router.php
@@ -1,28 +1,23 @@
 <?php
 
-//Call This Function In Your App Entry Point If You Are Building a Multiple PAge Application
 function ScreenRouter()
 {
-  //checking if linking is set
   if (isset($_GET['s'])) {
-    //checking if screen exist
     if (file_exists("screens/" . $_GET['s'] . "")) {
       require "screens/" . $_GET['s'] . "";
     } else {
       echo '
-              
-              <div style="min-height: 100vh;" class="page-wrap d-flex flex-row align-items-center">
-              <div class="container">
-                  <div class="row justify-content-center">
-                      <div class="col-md-12 text-center">
-                          <span class="display-1 d-block">404</span>
-                          <div class="mb-4 lead">The page you are looking for was not found.</div>
-                          <a href="./" class="btn btn-link">Back to Home</a>
-                      </div>
-                  </div>
+        <div style="min-height: 100vh;" class="page-wrap d-flex flex-row align-items-center">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-md-12 text-center">
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 lead">The page you are looking for was not found.</div>
+                <a href="./" class="btn btn-link" hx-get="index.php?s=home" hx-push-url="true" hx-target="#content">Back to Home</a>
               </div>
-            </div>    
-        ';
+            </div>
+          </div>
+        </div>';
     }
   } else {
     homepage();
